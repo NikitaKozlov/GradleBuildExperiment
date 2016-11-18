4b4d37289718539ab81de2fc1ec1cd85e0fd9e1b
Nikita Kozlov <nikita.e.kozlov@gmail.com>
2016-11-18 18:42:03 +0100

    Created forth activity and generated classes for 2nd and 4th modules

--- app2/src/main/java/com/nikitakozlov/secondmodule/SecondModule.java
+++ app2/src/main/java/com/nikitakozlov/secondmodule/SecondModule.java
@@ -1,9 +1,7 @@
 package com.nikitakozlov.secondmodule;
 
-import com.nikitakozlov.secondmodule.gen.package_0.Foo150;
 
 public class SecondModule {
     public void doSmth() {
-        new Foo150().foo4();
     }
 }
--- app4/src/main/java/com/nikitakozlov/forthmodule/ForthModule.java
+++
@@ -1,4 +0,0 @@
-package com.nikitakozlov.forthmodule;
-
-public class ForthModule {
-}
